f4e5c13a6559a24dd07ce87c042cdae2e8d57fd3
Nikhesh <nikhesh.p@kambala.co.in>
2022-12-22 11:17:41 +0530

    Removed unwanted code

--- example_market.py
+++ example_market.py
@@ -67,57 +67,15 @@ with open('cred.yml') as f:
 ret = api.login(userid = cred['user'], password = cred['pwd'], twoFA=cred['factor2'], vendor_code=cred['vc'], api_secret=cred['apikey'], imei=cred['imei'])
 
 if ret != None:   
-    while True:
-        print('f => find symbol')    
+    while True:  
         print('m => get quotes')
         print('p => contract info n properties')    
-        print('v => get 1 min market data')
-        print('t => get today 1 min market data')
-        print('d => get daily data')
-        print('o => get option chain')
-        print('s => start_websocket')
         print('q => quit')
 
         prompt1=input('what shall we do? ').lower()                    
         
-        if prompt1 == 'v':
-            start_time = "13-07-2021 09:10:00"
-            #end_time = time.time()
-            
-            start_secs = get_time(start_time)
 
-            end_time = get_time("13-07-2021 09:20:00")
-            ret = api.get_time_price_series(exchange='NSE', token='22', starttime=start_secs, endtime=end_time)
-            
-            df = pd.DataFrame.from_dict(ret)
-            print(df)            
-            print(f'{start_secs} to {end_time}')
-
-        elif prompt1 == 't':
-            ret = api.get_time_price_series(exchange='NSE', token='22')
-            
-            df = pd.DataFrame.from_dict(ret)
-            print(df)            
-            
-
-        elif prompt1 == 'f':
-            exch  = 'NFO'
-            query = 'BANKNIFTY 30DEC CE'
-            ret = api.searchscrip(exchange=exch, searchtext=query)
-            print(ret)
-
-            if ret != None:
-                symbols = ret['values']
-                for symbol in symbols:
-                    print('{0} token is {1}'.format(symbol['tsym'], symbol['token']))
-
-        elif prompt1 == 'd':
-            exch  = 'NSE'
-            tsym = 'RELIANCE-EQ'
-            ret = api.get_daily_price_series(exchange=exch, tradingsymbol=tsym, startdate=0)
-            print(ret)
-
-        elif prompt1 == 'p':
+        if prompt1 == 'p':
             exch  = 'NSE'
             token = '22'
             ret = api.get_security_info(exchange=exch, token=token)
@@ -127,24 +85,6 @@ if ret != None:
             exch  = 'NSE'
             token = '22'
             ret = api.get_quotes(exchange=exch, token=token)
-            print(ret)
-        elif prompt1 == 'o':
-            exch  = 'NFO'
-            tsym = 'COFORGE30DEC21F'
-            chain = api.get_option_chain(exchange=exch, tradingsymbol=tsym, strikeprice=3500, count=2)
-
-            chainscrips = []
-            for scrip in chain['values']:
-                scripdata = api.get_quotes(exchange=scrip['exch'], token=scrip['token'])
-                chainscrips.append(scripdata)
-
-            print(chainscrips)
-
-        elif prompt1 == 's':
-            if socket_opened == True:
-                print('websocket already opened')
-                continue
-            ret = api.start_websocket(order_update_callback=event_handler_order_update, subscribe_callback=event_handler_quote_update, socket_open_callback=open_callback)
             print(ret)
 
         else:
